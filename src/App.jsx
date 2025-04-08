43cac2e2fc2ae689a0f1f833186564ff9118e3b7
Leonidas MV <yordy.lmv.2000@gmail.com>
2025-04-08 16:45:16 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Importamos React Router
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
-import RecoverPassword from './components/RecoverPassword/RecoverPassword';
-import Map from '@/components/Map/Map'
-import CarView from '@/components/CarView/CarView';
-import UserSettings from '@/components/UserSettings/userSettings';
-import ResetPassword from './components/ResetPassword/ResetPassword'; // Asegúrate de tener esta página
-import Layout from './Layout';
-import Home from './components/Home/Home';
-import Map from './pages/map';
-import UserSettings from './pages/userSettings';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Importamos React Router
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import RecoverPassword from "./components/RecoverPassword/RecoverPassword";
+import Map from "@/components/Map/Map";
+import CarView from "@/components/CarView/CarView";
+import UserSettings from "@/components/UserSettings/userSettings";
+import ResetPassword from "./components/ResetPassword/ResetPassword"; // Asegúrate de tener esta página
+import Layout from "./Layout";
+import Home from "./components/Home/Home";
+// import Map from './pages/map';
+// import UserSettings from './pages/userSettings';
+import PoliceCarAlert from "./components/Alert"; // Asegúrate de tener esta página
 
 function App() {
   return (
-    <Router> {/* Envolvemos la app en Router */}
+    <Router>
+      {" "}
+      {/* Envolvemos la app en Router */}
       <Layout>
         <div className="App">
           <header className="App-header">
             <h1>Mi aplicación</h1>
 
-            <Routes> {/* Aquí definimos las rutas */}
+            <Routes>
+              {" "}
+              {/* Aquí definimos las rutas */}
               <Route path="/" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/recover-password" element={<RecoverPassword />} />
-              <Route path="/reset-password/:uidb64/:token" element={<ResetPassword />} />
-              <Route path="/map" element={<Map/>}/>
-              <Route path='/car-view' element={<CarView/>}/>
-              <Route path='/settings' element={<UserSettings/>}/>
+              <Route
+                path="/reset-password/:uidb64/:token"
+                element={<ResetPassword />}
+              />
+              <Route path="/map" element={<PoliceCarAlert />} />
+              <Route path="/car-view" element={<CarView />} />
+              <Route path="/settings" element={<UserSettings />} />
             </Routes>
           </header>
         </div>
